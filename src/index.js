63c4d70445e58c9ef849f8978d75ae792fb2536c
jennahakulinen <jenna.hakulinen@metropolia.fi>
2022-01-17 14:14:28 +0200

    Ulkoasu puuttuu, js toimii

--- src/index.js
+++ src/index.js
@@ -1,97 +1,3 @@
-  //TASK 2
-  // 'use strict';
-  // const min = 1;
-  // const max  = 100;
-  // const maxGuess = 10;
-  // let startTime = Date.now();
-
-  // let randomNumber = Math.floor(Math.random() * max) + min;
-  // const guesses = document.querySelector('.guesses');
-  // const lastResult = document.querySelector('.lastResult');
-  // const lowOrHi = document.querySelector('.lowOrHi');
-  // const guessSubmit = document.querySelector('.guessSubmit');
-  // const guessField = document.querySelector('.guessField');
-  // const timeField = document.querySelector('.time');
-
-  // let guessCount = 1;
-  // let resetButton;
-
-  // const checkGuess = () => {
-  //   const userGuess = Number(guessField.value);
-  //   if (guessCount === 1) {
-  //     guesses.textContent = 'Previous guesses: ';
-  //   }
-
-  //   guesses.textContent += userGuess + ' ';
-
-  //   if (userGuess === randomNumber) {
-  //     lastResult.textContent = 'Congratulations! You got it right!';
-  //     lastResult.style.backgroundColor = 'green';
-  //     lowOrHi.textContent = '';
-  //     stopTimer();
-  //     setGameOver();
-  //   } else if (guessCount === maxGuess) {
-  //     lastResult.textContent = '!!!GAME OVER!!!';
-  //     lowOrHi.textContent = '';
-  //     stopTimer();
-  //     setGameOver();
-  //   } else {
-  //     lastResult.textContent = 'Wrong!';
-  //     lastResult.style.backgroundColor = 'red';
-  //     if(userGuess < randomNumber) {
-  //       lowOrHi.textContent = 'Last guess was too low!' ;
-  //     } else if(userGuess > randomNumber) {
-  //       lowOrHi.textContent = 'Last guess was too high!';
-  //     }
-  //   }
-
-  //   guessCount++;
-  //   guessField.value = '';
-  //   guessField.focus();
-  // };
-
-  // guessSubmit.addEventListener('click', checkGuess);
-
-  // const setGameOver = () => {
-  //   guessField.disabled = true;
-  //   guessSubmit.disabled = true;
-  //   resetButton = document.createElement('button');
-  //   resetButton.textContent = 'Start new game';
-  //   document.body.appendChild(resetButton);
-  //   resetButton.addEventListener('click', resetGame);
-  // };
-
-  // const resetGame = () => {
-  //   guessCount = 1;
-  //   startTime = Date.now();
-  //   const resetParas = document.querySelectorAll('.resultParas p');
-  //   for (const resetPara of resetParas) {
-  //     resetPara.textContent = '';
-  //   }
-
-  //   timeField.textContent = '';
-
-  //   resetButton.parentNode.removeChild(resetButton);
-  //   guessField.disabled = false;
-  //   guessSubmit.disabled = false;
-  //   guessField.value = '';
-  //   guessField.focus();
-  //   lastResult.style.backgroundColor = 'white';
-  //   randomNumber = Math.floor(Math.random() * max) + min;
-  // };
-
-  // const stopTimer = () => {
-  //   const stopTime = Date.now() - startTime;
-
-  //   if (guessCount === maxGuess)  {
-  //     timeField.textContent += 'Your time: ' + Math.floor(stopTime / 1000) + ' seconds!';
-  //   }
-  //   else {
-  //     timeField.textContent += 'Your time: ' + Math.floor(stopTime / 1000) + ' seconds! It took you ' + guessCount + ' guesses.';
-  //   }
-  // };
-
-  //TASK 3
 
   'use strict';
 
@@ -104,16 +10,70 @@
     "Cheese / Chicken / Vege / Halloum burger and french fries"
   ];
 
-  const mainElem = document.querySelector(".container");
+  const coursesFi = [
+    "Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
+    "Goalaista kalacurrya ja täysjyväriisiä",
+    "Vegaani Chili sin carne ja täysjyväriisi",
+    "Parsakeittoa,lisäkesalaatti kahdella napaksella",
+    "Lunch baguette with BBQ-turkey filling",
+    "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"
+  ];
 
-  let htmlKoodi = ``;
+  const menu = document.querySelector('.menu');
+  const langBtn = document.querySelector('.lang');
+  const sortBtn = document.querySelector('.sort');
+  const rndBtn = document.querySelector('.random');
 
-  for (let i = 0; i < coursesEn.length; i++) {
-    htmlKoodi +=
-      `<div class="lista">
-          <li>${coursesEn[i]}</li>
-        </div>`;
-  }
+  let finnish = true;
 
-  mainElem.innerHTML = htmlKoodi;
+  coursesEn.sort();
+  coursesFi.sort();
 
+  coursesFi.forEach(annos => {
+    menu.innerHTML += '<li class="">' + annos +'</li>';
+  });
+
+  const changeLang = () => {
+    menu.innerHTML = '';
+    if (finnish) {
+      coursesEn.forEach(course => {
+        menu.innerHTML += '<li class="">' + course +'</li>';
+      });
+      finnish = false;
+    } else {
+      coursesFi.forEach(annos => {
+        menu.innerHTML += '<li class="">' + annos +'</li>';
+      });
+      finnish = true;
+    };
+  };
+
+  langBtn.addEventListener('click', changeLang);
+
+  const sortMenu = () => {
+    menu.innerHTML = '';
+    if (finnish) {
+      coursesFi.reverse();
+      coursesFi.forEach(annos => {
+        menu.innerHTML += '<li class="">' + annos +'</li>';
+      });
+    } else {
+      coursesEn.reverse();
+      coursesEn.forEach(course => {
+        menu.innerHTML += '<li class="">' + course +'</li>';
+    });
+  };
+};
+
+sortBtn.addEventListener('click', sortMenu);
+
+const randomCourse = () => {
+  const rand = Math.floor(Math.random()*coursesFi.length);
+  if (finnish) {
+    alert(coursesFi[rand]);
+  } else {
+    alert(coursesEn[rand]);
+  };
+};
+
+rndBtn.addEventListener('click', randomCourse);
