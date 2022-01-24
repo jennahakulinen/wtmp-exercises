c7930e80b61f92fec220741be7d6b9560a86b28e
jennahakulinen <jenna.hakulinen@metropolia.fi>
2022-01-24 11:32:29 +0200

    tunti esimerkit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-console.log('Hello console!');
+//Ajastus
+let counter = 0;
+
+const timer = setInterval(() => {
+  console.log("The tick");
+  counter++;
+  if (counter > 9) {
+    clearInterval(timer);
+    console.log('loppu');
+  }
+}, 500);
+
+console.log('kukkuu');
+console.log(timer);
+
+//Events
+const clickHandler = () => {
+  console.log('p clicked');
+};
+
+// document.querySelector('p').addEventListener('click', clickHandler );
+document.querySelector('p').addEventListener('click', event => {
+  console.log('p clicked', event);
+  if (event.altKey) {
+    console.log('p clicked with alt');
+    event.stopImmediatePropagation();
+  }
+});
+
+document.querySelector('#content').addEventListener('click', event => {
+  console.log('div clicked');
+});
+
+//Keyboard events
+
+let keyHistory = [];
+
+document.addEventListener('keydown', event => {
+  // console.log('key event', event); keyup, keypress
+  keyHistory.push(event.key);
+  if(event.key === 'Enter') {
+    console.log(keyHistory);
+    keyHistory = [];
+  }
+});
+
+// CustomEvent
+document.addEventListener('myMessage', event => {
+  console.log('got a message:', event.detail.msg);
+});
+const myevent = new CustomEvent('myMessage', { detail: {msg: 'Hello!'} });
+setInterval(() => {
+  document.dispatchEvent(myevent);
+}, 2000);
+
+//testing draggable.js
+import {Sortable} from '@shopify/draggable';
+// console.log(new Sortable());
+const sortable = new Sortable(document.querySelectorAll('ul'), {
+  draggable: 'li'
+});
