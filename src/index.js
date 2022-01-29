b85e910cc3f1be3bbea6e884280e55dff5100e53
jennahakulinen <jenna.hakulinen@metropolia.fi>
2022-01-29 12:42:40 +0200

    task3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import SodexoData from './modules/SodexoData';
-import FazerData from './modules/FazerData';
+import SodexoData from './modules/sodexo-data';
+import FazerData from './modules/fazer-data';
 
 let lang = 'fi';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
 
 /**
  * Sorts an array alphapetically
--- webpack.common.js
+++ webpack.common.js
@@ -3,12 +3,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: {
     app: './src/index.js',
   },
   plugins: [
+    new WebpackPwaManifest({
+      name: 'Lunch Progressive Web App',
+      short_name: 'LunchPWA',
+      description: 'Describe your Progressive Web App here',
+      background_color: '#ffffff',
+      crossorigin: 'use-credentials',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        },
+      ]
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
     new CleanWebpackPlugin(),
     new CopyPlugin({
       patterns: [
